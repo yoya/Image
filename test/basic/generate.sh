c54b340c2acb86673dbdfef02d47b6192f8669f5
Yoshihiro Yamazaki <yoya@awm.jp>
2024-02-22 20:30:06 +0900

    Exif Orientation 生成を orient.sh に分離。

--- test/basic/generate.sh
+++ test/basic/generate.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set -u
+set -euo pipefail
 
 # require)
 # - ImageMagick: https://imagemagick.org/
@@ -32,29 +32,9 @@ convert -size 100x75 -font .New-York-Italic -pointsize 64 \
 
 # Exif Orientation
 
-ORIGINAL_SUFFIX="_original" # exiftool の-delete-original が動かないので
-SUFFIX=".png"
+OUTPUT_ORIENT_DIR="$OUTPUT_DIR/orient"
 
-FILE=$OUTPUT_DIR/$SAMPLE_RGBY
-PREFIX=`basename $SAMPLE_RGBY $SUFFIX`
-
-for i in `seq 1 8` ;
-do
-  FILE_ORIENT="$OUTPUT_DIR/$PREFIX-$i$SUFFIX"
-  echo $FILE_ORIENT
-  cp $FILE $FILE_ORIENT
-  exiftool -Orientation=$i -n $FILE_ORIENT
-  rm $FILE_ORIENT$ORIGINAL_SUFFIX
-  case $i in
-    "2") mogrify             -flop $FILE_ORIENT ;;
-    "3") mogrify -rotate 180       $FILE_ORIENT ;;
-    "4") mogrify -rotate 180 -flop $FILE_ORIENT ;;
-    "5") mogrify -rotate -90 -flip $FILE_ORIENT ;;
-    "6") mogrify -rotate -90       $FILE_ORIENT ;;
-    "7") mogrify -rotate  90 -flip $FILE_ORIENT ;;
-    "8") mogrify -rotate  90       $FILE_ORIENT ;;
-  esac
-done
+sh ./orient.sh $SAMPLE_RGBY $OUTPUT_DIR $OUTPUT_ORIENT_DIR
 
 # DPI metadata
 
