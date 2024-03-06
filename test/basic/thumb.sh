4886e4a17b19d42f7a77372f03e52dfc3bbfae37
Yoshihiro Yamazaki <yoya@awm.jp>
2024-03-07 02:10:30 +0900

    refine

--- test/basic/thumb.sh
+++ test/basic/thumb.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 IM_CONV=convert
+PYTHON=python
 
 SAMPLE_RGBY=$1 ; OUTPUT_DIR=$2 ; OUTPUT_THUMB_DIR=$3
 mkdir -p $OUTPUT_THUMB_DIR
@@ -10,10 +11,10 @@ ORIGINAL_SUFFIX="_original" # exiftool の-delete-original が動かないので
 
 FILE=$OUTPUT_DIR/$SAMPLE_RGBY
 
-cp $FILE.jpg "$OUTPUT_THUMB_DIR/${SAMPLE_RGBY}-exifthumb.jpg"
 FILE_THUMB="$OUTPUT_THUMB_DIR/$SAMPLE_RGBY-exifthumb.jpg"
+cp $FILE.jpg $FILE_THUMB
 exiftool -q "-ThumbnailImage<=${FILE}.jpg" $FILE_THUMB
 rm $FILE_THUMB$ORIGINAL_SUFFIX
 
-python ../../python/jpegapp0_thumb.py -i ${FILE}.jpg ${FILE}.jpg > \
-         "$OUTPUT_THUMB_DIR/${SAMPLE_RGBY}-app0thumb.jpg"
+$PYTHON ../../python/jpegapp0_thumb.py -i ${FILE}.jpg ${FILE}.jpg > \
+        "$OUTPUT_THUMB_DIR/${SAMPLE_RGBY}-app0thumb.jpg"
