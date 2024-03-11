3d795d4ab33e6d5dcfa62591c454a4c9507f3933
Yoshihiro Yamazaki <yoya@awm.jp>
2024-03-11 18:38:19 +0900

    PNG32 を WebP 変換した時の罠の例として、サイズ比較。

---
+++ bash/webp_compare_jpeg_alpha.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+set -euo pipefail
+
+IM_COMMAND=convert
+SAMPLE=rose-vignette.png
+
+VIGNETTE_VALUE=0  # hard vignette
+# VIGNETTE_VALUE=32x32  # soft vignette
+
+$IM_COMMAND rose: -resize 240 \( +clone -fill white -colorize 100% \
+    -background black -vignette $VIGNETTE_VALUE \) -alpha off \
+    -compose copy_opacity -composite  $SAMPLE ;
+
+echo "quality,jpeg,webp(RGBA),webp"
+
+for q in `seq 10 10 90` ;
+do 
+    $IM_COMMAND rose-vignette.png -quality $q RGB-$q.jpg ;
+    $IM_COMMAND rose-vignette.png -define webp:emulate-jpeg-size=1 \
+                                  -quality $q RGBA-$q.webp ;
+    $IM_COMMAND rose-vignette.png -define webp:emulate-jpeg-size=1 -alpha off \
+                                  -quality $q RGB-$q.webp ;
+    size_jpeg=`identify -format %b  RGB-$q.jpg |  sed "s/[^0-9]//g"`
+    size_webpa=`identify -format %b  RGBA-$q.webp |  sed "s/[^0-9]//g"`
+    size_webp=`identify -format %b  RGB-$q.webp |  sed "s/[^0-9]//g"`
+    echo $q,$size_jpeg,$size_webpa,$size_webp
+done
