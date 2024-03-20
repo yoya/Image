cc4ebb3fd61aea3d129da13cb6741ca7f421be12
Yoshihiro Yamazaki <yoya@awm.jp>
2024-03-21 00:28:31 +0900

    640x480 と -quality 100 10000x10000 random を追加。

--- test/basic/size.sh
+++ test/basic/size.sh
@@ -8,6 +8,10 @@ mkdir -p $OUTPUT_SIZE_DIR
 for s in png jpg ; do
     $IM_CONVERT $OUTPUT_DIR/$SAMPLE_RGBY.png -sample "8x8!" \
                 "$OUTPUT_SIZE_DIR/${SAMPLE_RGBY}-8x8.$s"
+    $IM_CONVERT $OUTPUT_DIR/$SAMPLE_RGBY.png -sample "640x480!" \
+                "$OUTPUT_SIZE_DIR/${SAMPLE_RGBY}-640x480.$s"
     $IM_CONVERT -size 10000x10000 xc:black \
                 "$OUTPUT_SIZE_DIR/${SAMPLE_RGBY}-black-10000x10000.$s"
+    $IM_CONVERT -size 10000x10000 xc: +noise Random -quality 100 \
+                "$OUTPUT_SIZE_DIR/${SAMPLE_RGBY}-random-10000x10000-q100.$s"
 done
